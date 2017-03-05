58ad1841136203447c661e4b1d553fd89ed843b3
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2017-03-05 17:27:32 +0100

    initial commit

---
+++ src/Hander.php
@@ -0,0 +1,20 @@
+<?php
+namespace SlaxWeb\Cache;
+
+/**
+ * Cache Handler
+ *
+ * This abstract Cache Handler class defines functionality for handling data and
+ * defines abstract methods for functionality that each specific handler must implement.
+ * All cache handlers must extend from this abstract class.
+ *
+ * @package   SlaxWeb\Cache
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.1
+ */
+abstract class Handler
+{
+}
---
+++ src/Manager.php
@@ -0,0 +1,19 @@
+<?php
+namespace SlaxWeb\Cache;
+
+/**
+ * Cache Manager
+ *
+ * The Cache Manager class manages writes and reads from the cache through a cache
+ * handler on which it depends.
+ *
+ * @package   SlaxWeb\Cache
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.1
+ */
+class Manager
+{
+}
