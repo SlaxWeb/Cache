f2d1da15e576b8fb260ac4a19195aae15d4bd078
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2017-03-18 12:36:13 +0100

    add max age to config

--- config/cache.php
+++ config/cache.php
@@ -32,3 +32,13 @@ $configuration["handler"] = "file";
  * to the directory where the Cache component may write to.
  */
 $configuration["location"] = __DIR__ . "/../Cache";
+
+/*
+ * Maximum age
+ *
+ * Maximum age for data in the cache. This value is used as default for all data
+ * stored in the cache, but can be overriden for each one specifically. Maximum
+ * age is defined in seconds with a default value of int(3600), equivalent for one
+ * hour.
+ */
+$configuration["maxAge"] = 3600;
