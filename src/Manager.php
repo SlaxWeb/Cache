3ef9bed1fa30819055f61086db1477b993179a09
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2017-03-18 12:35:59 +0100

    add the write and read functionality to the manager

--- src/Manager.php
+++ src/Manager.php
@@ -16,4 +16,58 @@ namespace SlaxWeb\Cache;
  */
 class Manager
 {
+    /**
+     * Cache handler
+     *
+     * @var \SlaxWeb\Cache\AbstractHandler
+     */
+    protected $handler = null;
+
+    /**
+     * Constructor
+     *
+     * Class constructor copies the dependencies to the class properties. The Cache
+     * Manager depends on the cache handler object, and uses it throughout its execution
+     * to write and read from the cache.
+     *
+     * @param \SlaxWeb\Cache\AbstractHandler $handler Cache handler object
+     */
+    public function __construct(AbstractHandler $handler)
+    {
+        $this->handler = $handler;
+    }
+
+    /**
+     * Write
+     *
+     * Serializes the data and writes it to cache using the handler. It accepts
+     * the name for the cached data as the first parameter, the data as the second
+     * parameter, and a maximum age value as the third parameter. If the maximum
+     * age is not supplied or is of a negative value, the configured default is
+     * used. If an error occurs when trying to write to cahce, bool(false) is returned,
+     * bool(true) otherwise.
+     *
+     * @param string $name Name of the data to be written to cache
+     * @param mixed $data Data to be stored into the cache
+     * @param int $maxAge Maximum age for this specific data, default -1
+     * @return bool
+     */
+    public function write(string $name, $data, int $maxAge = -1): bool
+    {
+        return $this->handler->write($name, serialize($data), $maxAge);
+    }
+
+    /**
+     * Read
+     *
+     * Reads the data from the cache and returns. It does not handle any handler
+     * thrown exceptions, and simply returns that what it receives.
+     *
+     * @param string $name Name of the data stored in cache
+     * @return mixed
+     */
+    public function read(string $name)
+    {
+        return unserialize($this->handler->get($name));
+    }
 }
